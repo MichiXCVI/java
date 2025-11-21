bd01fb7f5bce62fa1faba5fa2915969bd06c97b1
Ronca <michironca08@gmail.com>
2025-11-21 08:29:12 +0100

    .

--- forme/src/Triangolo.java
+++ forme/src/Triangolo.java
@@ -14,10 +14,7 @@ public class Triangolo {
         this.perimetro = lato + lato1 + lato2;
         if (lato + lato1 > lato2 && lato + lato2 > lato1 && lato1 + lato2 > lato) {
             double semiperimetro = perimetro / 2.0;
-            this.area = sqrt(semiperimetro *
-                    (semiperimetro - lato) *
-                    (semiperimetro - lato1) *
-                    (semiperimetro - lato2));
+            this.area = sqrt(semiperimetro * (semiperimetro - lato) * (semiperimetro - lato1) * (semiperimetro - lato2));
         } else {
             System.out.println("I valori inseriti NON formano un triangolo valido.");
         }
